4a0b8226a44000be3032b0e75d5be7ad32c5108a
StefanBratanov <stefan.bratanov93@gmail.com>
2026-01-28 16:56:57 +0000

    renames

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/validation/GossipValidationHelper.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/validation/GossipValidationHelper.java
@@ -89,7 +89,7 @@ public class GossipValidationHelper {
     final UInt64 firstSlotInBlockEpoch =
         spec.computeStartSlotAtEpoch(spec.computeEpochAtSlot(slot));
     return parentBlockSlot.isLessThan(firstSlotInBlockEpoch)
-        ? recentChainData.retrieveStateAtSlot(
+        ? recentChainData.retrieveBlockState(
             new SlotAndBlockRoot(firstSlotInBlockEpoch, parentBlockRoot))
         : recentChainData.retrieveBlockState(parentBlockRoot);
   }
@@ -140,7 +140,7 @@ public class GossipValidationHelper {
 
   public SafeFuture<Optional<BeaconState>> getStateAtSlotAndBlockRoot(
       final SlotAndBlockRoot slotAndBlockRoot) {
-    return recentChainData.retrieveStateAtSlot(slotAndBlockRoot);
+    return recentChainData.retrieveBlockState(slotAndBlockRoot);
   }
 
   public boolean isValidatorInPayloadTimelinessCommittee(
